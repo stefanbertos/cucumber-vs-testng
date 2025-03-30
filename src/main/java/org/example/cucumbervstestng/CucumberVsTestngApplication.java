1b3c96c2d92e98881378177c56201a798b3096e0
Stefan <stefan.bertos@gmail.com>
2025-03-30 18:48:50 +0200

    initial draft

---
+++ src/main/java/org/example/cucumbervstestng/CucumberVsTestngApplication.java
@@ -0,0 +1,13 @@
+package org.example.cucumbervstestng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberVsTestngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberVsTestngApplication.class, args);
+    }
+
+}
