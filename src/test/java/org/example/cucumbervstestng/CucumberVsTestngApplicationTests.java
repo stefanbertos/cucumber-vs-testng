0149012d73ccfe19d4b3b53d6c689cf743632554
Stefan <stefan.bertos@gmail.com>
2025-03-30 18:48:50 +0200

    initial draft

---
+++ src/test/java/org/example/cucumbervstestng/CucumberVsTestngApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cucumbervstestng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberVsTestngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
